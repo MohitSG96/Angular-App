e0d8155519291a1e4924c54e8a626c89df90dd25
Mohit <mohit.g.mg1996@gmail.com>
2019-08-07 19:47:35 +0530

    Final website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ItemsModule } from './items/items.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    ItemsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
